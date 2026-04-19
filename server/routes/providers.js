14d5d00e65335aaab9234f75c4af576416e6b515
Saumyaranjan Nayak <107764221+Sdnsoumy@users.noreply.github.com>
2026-04-19 09:42:51 +0530

    fix: remove unnecessary authentication middleware from nearby providers route

--- server/routes/providers.js
+++ server/routes/providers.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 const { protect, authorize } = require('../middlewares/auth');
 const { getNearbyProviders, registerAsProvider } = require('../controllers/providerController');
 
-router.get('/nearby', protect, getNearbyProviders);
+router.get('/nearby', getNearbyProviders);
 router.post('/register', protect, authorize('provider'), registerAsProvider);
 
 module.exports = router;
